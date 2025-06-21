c9f418bc9f797944069e08e08adb8b782aa536f1
jakubg1 <24206305+jakubg1@users.noreply.github.com>
2025-06-21 12:42:06 +0200

    Initial Commit

---
+++ src/main/java/pl/pja/jaz_s32362_nbp/JazS32362NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pja.jaz_s32362_nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS32362NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS32362NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pja/jaz_s32362_nbp/JazS32362NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pja.jaz_s32362_nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS32362NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
